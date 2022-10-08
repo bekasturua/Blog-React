1f463b3a4cd74ead3a46e6406279305dffb28a84
Beka Sturua <beqasturua3@gmail.com>
2022-10-08 17:59:29 +0400

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const person = { name: "Beka", age: 21 };
-  const link = "https://www.google.com.br/";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p> */}
-
-        <p>{10}</p>
-        <p>{"Hello,Beka"}</p>
-        <p>{[1, 2, 3, 4, 5, 6]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}>Google</a>
+        <Home />
       </div>
     </div>
   );
