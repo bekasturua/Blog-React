2595e294bfe63fcf962fe28b55e3dcc312c8b75c
Beka Sturua <beqasturua3@gmail.com>
2022-10-10 22:53:25 +0400

    fix fetch adress bug

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      fetch("http://localhost:8000/blogss")
+      fetch("http://localhost:8000/blogs")
         .then((res) => {
           if (!res.ok) {
             throw Error("Could not fetch the data for that resource");
